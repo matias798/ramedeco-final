dae0380823e7023a47331c2b8ef7949a00d449a0
ezequielRiveros <eriveros298@gmail.com>
2020-11-20 20:31:28 -0300

    fixing project since it couldn't start

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000,"Server Started Successfully")
+app.listen(3000,()=>console.log("server started"))
 
-app.get("/home", (req,res) =>{
+app.get("/", (req,res) =>{
     res.sendFile(__dirname+"/views/home.html") 
 })
 /*
