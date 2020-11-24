f7c9159ba23a97f3d6fc44a01c1224d57306242f
matias798 <matiquiro.mq@gmail.com>
2020-11-23 23:03:47 -0300

    added constant port  in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const port=3000;
 
-app.listen(3000,()=>console.log("server started"))
+app.listen(port,()=>console.log("server started at Port: "+port))
 
 app.get("/", (req,res) =>{
     res.sendFile(__dirname+"/views/home.html") 
