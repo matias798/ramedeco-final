fdf9089d121d62d64fb66c3fe512a60764a84426
ezequielRiveros <eriveros298@gmail.com>
2021-01-14 20:40:44 -0300

    Adding Bcrypt to Register and Login

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -1,5 +1,7 @@
 const users =require('../data/users.json')
 const fs =require('fs')
+const bcrypt=require('bcrypt')
+
 const user_template ={
     "first_name": "",
     "last_name": "",
@@ -19,7 +21,7 @@ module.exports={
         let username=req.body.username
         let password=req.body.password
         let user=users.find(user => {return user.username == username})
-        if(user.password===password){
+        if(bcrypt.compare(password,user.password)){
             if(user.role === "admin"){
                 res.redirect('/products')
             }else res.redirect('/')
@@ -35,6 +37,7 @@ module.exports={
         let user = {...user_template,...req.body}
         user.role="user"        
         user.id=users[users.length-1].id +1
+        user.password=bcrypt.hashSync(user.password,10)
         users.push(user)
         fs.writeFileSync(pathUserJSON,JSON.stringify(users))
         res.redirect('/')
