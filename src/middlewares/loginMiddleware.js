21ee532179f787971238d9221961347eb9b52354
Agustina Sánchez Ré <agustinasanchezre@gmail.com>
2021-01-19 11:05:13 -0300

    login middleware ready

--- src/middlewares/loginMiddleware.js
+++ src/middlewares/loginMiddleware.js
@@ -1,9 +1,11 @@
 //este middleware es local, lo voy a requerir solo en las rutas en las que haga falta
 
 function loginMiddleware(req,res,next){
-
-    next();
-
+    if(req.session.userLogged == undefined){
+        res.render('login');
+    }else{
+            next();
+        }        
 }
 
 module.exports = loginMiddleware;
