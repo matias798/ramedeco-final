d3416571b37a4178af2405c20a23711b5e4c4c82
eliett1 <eliett1@gmail.com>
2021-02-06 19:29:03 -0300

    Adding datatypes to Image Model

--- src/database/models/Image.js
+++ src/database/models/Image.js
@@ -1,25 +1,35 @@
 module.exports=function(sequelize,DataTypes){
-    const Image = sequelize.define("images", {
-    id: {
-        type: DataTypes.BIGINT,
-        autoIncrement:true, 
-        primaryKey: true
-    },
-    path: {
-        type: DataTypes.STRING(64)
-    },
-    create_at: {
-        type: DataTypes.DATE
-    },
-    updated_at: {
-        type: DataTypes.DATE
-    },
-    deleted_at: {
-        type: DataTypes.DATE
-    },
-})
-Image.associate = function (models) {
-    Image.belongsTo(models.products, {as: "product", foreignKey: "product_id"})
-}
+    const Image = sequelize.define("images", 
+    
+    {
+        id: {
+            type: DataTypes.BIGINT(20),
+            autoIncrement:true, 
+            primaryKey: true
+        },
+        path: {
+            type: DataTypes.STRING(64),
+            allowNull: false
+        },
+        create_at: {
+            type: DataTypes.DATE
+        },
+        updated_at: {
+            type: DataTypes.DATE
+        },
+        deleted_at: {
+            type: DataTypes.DATE
+        },
+       
+   })
 
-return Image}
+ Image.associate = function (models) {
+    Image.belongsTo(models.products, {
+        as: "product", 
+        foreignKey: "product_id_image"
+    })
+ }
+ 
+ return Image
+
+}
