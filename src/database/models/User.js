a3418fffb23d7aedda84bd518e421bbcf81727e0
eliett1 <eliett1@gmail.com>
2021-02-06 19:04:03 -0300

    adding dataTypes to User Model

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -3,24 +3,33 @@ module.exports = function (sequelize, DataTypes) {
 
         {
             id: {
-                type: DataTypes.SMALLINT,
+                type: DataTypes.BIGINT(20),
                 autoIncrement: true,
                 primaryKey: true
             },
             first_name: {
-                type: DataTypes.STRING(64)
+                type: DataTypes.STRING(64),
+                allowNull: false
             },
             last_name: {
-                type: DataTypes.STRING(64)
+                type: DataTypes.STRING(64),
+                allowNull: false
             },
             email: {
-                type: DataTypes.STRING(64)
+                type: DataTypes.STRING(64),
+                allowNull: false
             },
             password: {
-                type: DataTypes.STRING(100)
+                type: DataTypes.STRING(100),
+                allowNull: false
             },
             username: {
-                type: DataTypes.STRING(64)
+                type: DataTypes.STRING(64),
+                allowNull: false
+            },
+            address: {
+                type: DataTypes.STRING(64),
+                allowNull: false
             },
             avatar: {
                 type: DataTypes.STRING(64)
@@ -34,6 +43,8 @@ module.exports = function (sequelize, DataTypes) {
             deleted_at: {
                 type: DataTypes.DATE
             },
+            
+
         })
 
     User.associate = function (models) {
