72e33c8dad9f735ada22549ed73cbd8efabafe0e
eliett1 <eliett1@gmail.com>
2021-03-12 11:19:16 -0300

    adding minor details

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', function() {
+
+      //Considerar campos Usuario y Contraseña
    
       //Error de no escribir un nombre de usuario
       let userError = document.querySelector(".userError");
@@ -12,33 +14,48 @@ window.addEventListener('load', function() {
       // Evento de enviar formulario
       sendForm.addEventListener('submit',function(e){
 
+            // Array para errores
             let errors = []
 
+            //input de usuario
             let user = document.querySelector("input.user");
 
+            //input de password
             let password = document.querySelector("input.writePassword");
 
+                  // Si el usuario está vacío
                   if (user.value == '') {
+                       
+                        //Se suma un error
                         errors++;
 
-                        userError.innerHTML = "Debes escribir un usuario";
+                        //El error se muestra en el HTML
+                        userError.innerHTML = "Por favor escribe tu nombre de usuario";
                   } else {
 
+                        //no se muestra error
                         userError.innerHTML = "";
                   }
 
 
+                  //Si la contraseña está vacía
                   if (password.value == '') {
+                       
+                       //Se suma un error
                         errors++;
 
+                        //se muestra el error 
                         passwordError.innerHTML = "Debes escribir el password";
                   } else {
 
+                        //No muestro ningún error
                         passwordError.innerHTML = "";
                   }
 
+                  //Si existe un error
                   if (errors > 0) {
 
+                        //No se envía el formulario
                         e.preventDefault()
                   }
    
