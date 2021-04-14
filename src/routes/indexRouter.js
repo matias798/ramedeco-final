6566a6a2944f77ff15218e46b71bd577965c0900
ezequielRiveros <eriveros298@gmail.com>
2021-04-14 17:45:06 -0300

    Fixing erros in adming home

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -6,5 +6,5 @@ router.get('/', productsContoller.index);
 router.post('/search', productsContoller.search);
 router.get('/info', productsContoller.info);
 router.get('/contact', productsContoller.contact);
-
+router.get('/userhome', productsContoller.indexUser);
 module.exports = router;
